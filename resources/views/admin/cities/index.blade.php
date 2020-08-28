09517d935f39bfe01024b476f8adadc888e9b5c3
Clint <csbenavidez@circuit-solutions.net>
2020-08-28 17:27:04 +0800

    added index,create,edit blade and crud function

---
+++ resources/views/admin/cities/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Cities</div>
+
+                <div class="card-body">
+                    <a href="{{ route('admin.cities.create') }}" class="btn btn-primary mb-3">Add new</a>
+
+                    <table class="table table-bordered text-center">
+                        <tr>
+                            <th>Name</th>
+                            <th>Action</th>
+                        </tr>
+                        @forelse($cities as $city)
+                            <tr>
+                                <td>{{ $city->name }}</td>
+                                <td>
+                                    <a href="{{ route('admin.cities.edit', $city->id) }}" class="btn btn-secondary">
+                                        Edit
+                                    </a>
+                                    <form method="POST" action="{{ route('admin.cities.destroy', $city->id) }}">
+                                    {{ method_field('DELETE') }}
+                                    @csrf
+                        
+                                        <input type="submit" value="Delete" onclick="return confirm('Are you sure?')" class="btn btn-danger" /> 
+                                    </form>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="2">
+                                    No records found.
+                                </td>
+                            </tr>
+                        @endforelse
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
